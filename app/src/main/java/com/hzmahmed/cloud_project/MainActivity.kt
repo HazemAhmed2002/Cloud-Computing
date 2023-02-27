8b238ea620f4dd4dc3e71328935a4a1bfb5db1ce
HazemAhmed2002 <ahmedhazeml734@gmail.com>
2023-02-27 13:47:15 +0200

    first commit

---
+++ app/src/main/java/com/hzmahmed/cloud_project/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.hzmahmed.cloud_project
+
+import android.content.ContentValues.TAG
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.hzmahmed.cloud_project.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    val db = Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.save.setOnClickListener{
+            val name = binding.PersonName.text.toString()
+            val id = binding.PersonId.text.toString()
+            val age = binding.PersonAge.text.toString()
+
+            val person = hashMapOf(
+                "name" to name,
+                "id" to id,
+                "age" to age,
+            )
+            db.collection("persons")
+                .add(person)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }.addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+
+            }
+
+
+    }
+}
