e863f7ad3a8e208470243c317dec8c2c3447c0f7
HazemAhmed2002 <ahmedhazeml734@gmail.com>
2023-02-27 15:31:53 +0200

    new Edit

--- app/src/main/java/com/hzmahmed/cloud_project/MainActivity.kt
+++ app/src/main/java/com/hzmahmed/cloud_project/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.hzmahmed.cloud_project
 
 import android.content.ContentValues.TAG
-import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
